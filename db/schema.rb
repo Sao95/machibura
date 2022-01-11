853d220e81876b679bc360d7a97237082a98a3e0
Sao95 <merasaori@gmail.com>
2022-01-11 09:00:02 +0000

    [Add]Topページ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_11_063124) do
+ActiveRecord::Schema.define(version: 2022_01_11_082249) do
+
+  create_table "homes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
