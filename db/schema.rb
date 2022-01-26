fe631baab9342ba8d165d11ac717ef7aa363c218
Sao95 <merasaori@gmail.com>
2022-01-26 16:39:14 +0000

    [Fix]

--- db/schema.rb
+++ db/schema.rb
@@ -66,22 +66,6 @@ ActiveRecord::Schema.define(version: 2022_01_26_085622) do
     t.float "longitude"
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "user_roles", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "role_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["role_id"], name: "index_user_roles_on_role_id"
-    t.index ["user_id", "role_id"], name: "index_user_roles_on_user_id_and_role_id", unique: true
-    t.index ["user_id"], name: "index_user_roles_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
