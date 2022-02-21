7511c50071866f8aa7cdcbd13ee6206dcd2fd2d0
Sao95 <merasaori@gmail.com>
2022-02-21 09:35:13 +0000

    [Add]投稿のユーザーネーム

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 module ApplicationHelper
-  
+
   def user_name(user)
     if user.nil?
       # ログインしていないユーザーの表示名を設定
       '街ブラさん'
     else
       if admin_signed_in?
+        # user.nameにユーザー詳細へのリンクを設定
         content_tag(:a, :href => "/admin/users/#{user.id}") do
           concat user.name
         end
@@ -17,5 +18,5 @@ module ApplicationHelper
       end
     end
   end
-  
+
 end
